d480c149da0b3796fe55d52c28e7eb4113792c8b
Conor Heine <conor.heine@gmail.com>
2014-06-18 01:12:29 -0700

    installing custom moonc to travis

--- .travis_setup.sh
+++ .travis_setup.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-# Install openresty
-git clone https://github.com/openresty/ngx_openresty.git 
-cd ngx_openresty
-make
-cd ngx_openresty*
-./configure --with-luajit --with-http_postgres_module -j4
-make
-sudo make install
+SCRIPT_ROOT=`pwd`
+
+install_moonscript() {
+  git clone https://github.com/cahna/moonscript.git
+  cd moonscript
+  sudo luarocks make
+  cd "$SCRIPT_ROOT"
+}
+
+install_openresty() {
+  git clone https://github.com/openresty/ngx_openresty.git 
+  cd ngx_openresty
+  make
+  cd ngx_openresty*
+  ./configure --with-luajit --with-http_postgres_module -j4
+  make
+  sudo make install
+}
+
+install_moonscript
+install_openresty
+
